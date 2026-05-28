9a601feec53af6329eeaaeb43fb99dabd31c3213
André Cabrita <andrefmcabrita@gmail.com>
2026-05-28 03:47:15 +0100

    chore: scaffold global config packages

---
+++ packages/config-eslint/index.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+];
